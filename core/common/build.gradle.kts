d0b913fbab2d42b6cf62f65c05fb75b50b470560
soyeonLee126 <soyeon23949@gmail.com>
2026-01-20 16:43:12 +0900

    chore: Core Common 모듈 리소스 및 설정 추가
    - 공통 문자열 리소스 추가
    - build.gradle.kts 설정 업데이트

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -30,5 +32,8 @@ dependencies {
     implementation(libs.kotlinx.coroutines.android)
 
     // Testing
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+
     testImplementation(libs.junit)
 }
