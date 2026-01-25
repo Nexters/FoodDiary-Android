e008fc00725032c136fbcf2774aaf5237457306a
Hoseong-Ryu <hoseong091234@gmail.com>
2026-01-25 21:50:06 +0900

    refactor: 사진 쿼리 로직 개선 및 `DATE_TAKEN` 사용

--- data/src/main/java/com/nexters/fooddiary/data/datasource/LocalMediaDataSource.kt
+++ data/src/main/java/com/nexters/fooddiary/data/datasource/LocalMediaDataSource.kt
@@ -24,45 +24,7 @@ class LocalMediaDataSource @Inject constructor(
      */
     suspend fun getAllPhotos(): Map<LocalDate, List<PhotoData>> =
         withContext(Dispatchers.IO) {
-            val photosByDate = mutableMapOf<LocalDate, MutableList<PhotoData>>()
-
-            // 쿼리할 컬럼
-            val projection = arrayOf(
-                Media._ID,
-                Media.DATE_MODIFIED
-            )
-
-            // 최신순 정렬
-            val sortOrder = "${Media.DATE_MODIFIED} DESC"
-
-            try {
-                contentResolver.query(
-                    Media.EXTERNAL_CONTENT_URI,
-                    projection,
-                    null,  // 조건 없음 - 전체 스캔
-                    null,
-                    sortOrder
-                )?.use { cursor ->
-                    val idColumn = cursor.getColumnIndexOrThrow(Media._ID)
-                    val dateModifiedColumn = cursor.getColumnIndexOrThrow(Media.DATE_MODIFIED)
-
-                    while (cursor.moveToNext()) {
-                        val id = cursor.getLong(idColumn)
-                        val dateModified = cursor.getLong(dateModifiedColumn)
-
-                        val photo = PhotoData(
-                            uri = ContentUris.withAppendedId(Media.EXTERNAL_CONTENT_URI, id),
-                            dateTaken = dateModified
-                        )
-
-                        val photoDate = photo.dateTaken.toLocalDate()
-                        photosByDate.getOrPut(photoDate) { mutableListOf() }.add(photo)
-                    }
-                }
-            } catch (e: Exception) {
-                //TODO 에러 핸들링
-            }
-            photosByDate
+            queryPhotos()
         }
 
     /**
@@ -73,17 +35,9 @@ class LocalMediaDataSource @Inject constructor(
             val startDate = yearMonth.atDay(1)
             val endDate = yearMonth.atEndOfMonth()
 
-            val photosByDate = mutableMapOf<LocalDate, MutableList<PhotoData>>()
-
             // LocalDate를 Unix timestamp (초)로 변환
             val startTimestamp = startDate.atStartOfDay(ZoneId.systemDefault()).toEpochSecond()
             val endTimestamp = endDate.plusDays(1).atStartOfDay(ZoneId.systemDefault()).toEpochSecond()
-
-            // 쿼리할 컬럼
-            val projection = arrayOf(
-                Media._ID,
-                Media.DATE_MODIFIED
-            )
 
             // 날짜 범위 조건
             val selection = "${Media.DATE_MODIFIED} >= ? AND ${Media.DATE_MODIFIED} < ?"
@@ -92,38 +46,64 @@ class LocalMediaDataSource @Inject constructor(
                 endTimestamp.toString()
             )
 
-            // 최신순 정렬
-            val sortOrder = "${Media.DATE_MODIFIED} DESC"
-
-            try {
-                contentResolver.query(
-                    Media.EXTERNAL_CONTENT_URI,
-                    projection,
-                    selection,
-                    selectionArgs,
-                    sortOrder
-                )?.use { cursor ->
-                    val idColumn = cursor.getColumnIndexOrThrow(Media._ID)
-                    val dateModifiedColumn = cursor.getColumnIndexOrThrow(Media.DATE_MODIFIED)
-
-                    while (cursor.moveToNext()) {
-                        val id = cursor.getLong(idColumn)
-                        val dateModified = cursor.getLong(dateModifiedColumn)
-
-                        val photo = PhotoData(
-                            uri = ContentUris.withAppendedId(Media.EXTERNAL_CONTENT_URI, id),
-                            dateTaken = dateModified
-                        )
-
-                        val photoDate = photo.dateTaken.toLocalDate()
-                        photosByDate.getOrPut(photoDate) { mutableListOf() }.add(photo)
-                    }
-                }
-            } catch (e: Exception) {
-                //TODO 에러 핸들링
-            }
-            photosByDate
+            queryPhotos(selection, selectionArgs)
         }
+
+    private fun queryPhotos(
+        selection: String? = null,
+        selectionArgs: Array<String>? = null
+    ): Map<LocalDate, List<PhotoData>> {
+        val photosByDate = mutableMapOf<LocalDate, MutableList<PhotoData>>()
+
+        // 쿼리할 컬럼
+        val projection = arrayOf(
+            Media._ID,
+            Media.DATE_TAKEN,
+            Media.DATE_MODIFIED
+        )
+
+        // 최신순 정렬
+        val sortOrder = "${Media.DATE_MODIFIED} DESC"
+
+        try {
+            contentResolver.query(
+                Media.EXTERNAL_CONTENT_URI,
+                projection,
+                selection,
+                selectionArgs,
+                sortOrder
+            )?.use { cursor ->
+                val idColumn = cursor.getColumnIndexOrThrow(Media._ID)
+                val dateTakenColumn = cursor.getColumnIndexOrThrow(Media.DATE_TAKEN)
+                val dateModifiedColumn = cursor.getColumnIndexOrThrow(Media.DATE_MODIFIED)
+
+                while (cursor.moveToNext()) {
+                    val id = cursor.getLong(idColumn)
+                    val dateTaken = cursor.getLong(dateTakenColumn)
+                    val dateModified = cursor.getLong(dateModifiedColumn)
+
+                    // DATE_TAKEN이 있으면(0보다 크면) 사용하고, 없으면 DATE_MODIFIED 사용
+                    // DATE_TAKEN은 ms 단위이므로 초 단위로 변환 필요
+                    val timestamp = if (dateTaken > 0) {
+                        dateTaken / 1000
+                    } else {
+                        dateModified
+                    }
+
+                    val photo = PhotoData(
+                        uri = ContentUris.withAppendedId(Media.EXTERNAL_CONTENT_URI, id),
+                        dateTaken = timestamp
+                    )
+
+                    val photoDate = photo.dateTaken.toLocalDate()
+                    photosByDate.getOrPut(photoDate) { mutableListOf() }.add(photo)
+                }
+            }
+        } catch (e: Exception) {
+            //TODO 에러 핸들링
+        }
+        return photosByDate
+    }
 
     data class PhotoData(
         val uri: Uri,
