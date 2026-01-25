000629ad2b04511c82f80a8429bd23b47ebc3327
Hoseong-Ryu <hoseong091234@gmail.com>
2026-01-25 22:53:34 +0900

    chore: SDK 버전을 버전 카탈로그와 동기화

--- presentation/component/build.gradle.kts
+++ presentation/component/build.gradle.kts
@@ -6,10 +6,10 @@ plugins {
 
 android {
     namespace = "com.nexters.fooddiary.presentation.component"
-    compileSdk = 36
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
-        minSdk = 26
+        minSdk = libs.versions.minSdk.get().toInt()
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
