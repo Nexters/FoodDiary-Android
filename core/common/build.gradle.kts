dfcc9729462a183b95f7e85a29b4278d07a3e5c1
Hoseong-Ryu <hoseong091234@gmail.com>
2026-01-25 22:12:53 +0900

    feat: PermissionUtil 추가를 위한 android 종속성 추가

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -1,8 +1,31 @@
 plugins {
-    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.nexters.fooddiary.core.common"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
+    // Kotlin
+    implementation(libs.androidx.core.ktx)
+
     // Coroutines
     implementation(libs.kotlinx.coroutines.core)
 
