adaa9a4275ec30ae2eb8f49d3a7267d1de43f2b0
Hoseong-Ryu <hoseong091234@gmail.com>
2026-02-01 16:21:24 +0900

    feat: 토큰 유효성 검증 API 추가

--- data/src/main/java/com/nexters/fooddiary/data/remote/auth/AuthApi.kt
+++ data/src/main/java/com/nexters/fooddiary/data/remote/auth/AuthApi.kt
@@ -2,10 +2,14 @@ package com.nexters.fooddiary.data.remote.auth
 
 import com.nexters.fooddiary.data.remote.auth.model.request.LoginRequest
 import com.nexters.fooddiary.data.remote.auth.model.response.LoginResponse
+import com.nexters.fooddiary.data.remote.auth.model.response.VerifyTokenResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthApi {
     @POST("/auth/login")
     suspend fun login(@Body request: LoginRequest): LoginResponse
+
+    @POST("/auth/verify")
+    suspend fun verifyToken(): VerifyTokenResponse
 }
