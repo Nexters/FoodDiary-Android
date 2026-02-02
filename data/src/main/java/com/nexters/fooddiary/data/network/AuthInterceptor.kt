974ad3b20cd12990d0589c6511fad3038ce85760
Hoseong-Ryu <hoseong091234@gmail.com>
2026-02-02 18:58:46 +0900

    feat: AuthInterceptor 추가

---
+++ data/src/main/java/com/nexters/fooddiary/data/network/AuthInterceptor.kt
@@ -0,0 +1,40 @@
+package com.nexters.fooddiary.data.network
+
+import com.nexters.fooddiary.data.local.TokenStore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+
+class AuthInterceptor @Inject constructor(
+    private val tokenStore: TokenStore
+) : Interceptor {
+
+    companion object {
+        private const val HEADER_AUTHORIZATION = "Authorization"
+        private const val LOGIN_PATH = "/auth/login"
+    }
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+
+        if (originalRequest.url.encodedPath == LOGIN_PATH) {
+            return chain.proceed(originalRequest)
+        }
+
+        val token = runBlocking(Dispatchers.IO) {
+            tokenStore.getToken()
+        }
+
+        if (token.isNullOrBlank()) {
+            return chain.proceed(originalRequest)
+        }
+
+        val authenticatedRequest = originalRequest.newBuilder()
+            .header(HEADER_AUTHORIZATION, "Bearer $token")
+            .build()
+
+        return chain.proceed(authenticatedRequest)
+    }
+}
