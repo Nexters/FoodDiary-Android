2cc9af5312d10704caa6f91ca2727f90f3938daf
Hoseong-Ryu <hoseong091234@gmail.com>
2026-02-08 17:38:20 +0900

    feat: 온보딩 화면에 statusBarsPadding 추가

--- presentation/onboarding/src/main/java/com/nexters/fooddiary/presentation/onboarding/OnboardingScreen.kt
+++ presentation/onboarding/src/main/java/com/nexters/fooddiary/presentation/onboarding/OnboardingScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
@@ -62,6 +63,7 @@ internal fun OnboardingScreen(
         modifier = modifier
             .fillMaxSize()
             .background(OnboardingBackgroundColor)
+            .statusBarsPadding()
             .navigationBarsPadding()
     ) {
         // 중앙 - HorizontalPager
