2e3ace031e9d4776b41518256b709870399f9342
soyeonLee126 <soyeon23949@gmail.com>
2026-02-11 13:44:58 +0900

    - sentry 추가 - test crash 버튼 추가
    # Conflicts:
    #	app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -186,7 +186,9 @@ dependencies {
     // Firebase Cloud Messaging
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.messaging)
+    implementation(libs.sentry.android)
 
+    // sentry
     implementation(libs.sentry.android)
 
     // Testing
