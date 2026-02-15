9c59f5ab31ce95908e9a35ad62623d5c90a5b104
Hoseong-Ryu <hoseong091234@gmail.com>
2026-02-15 10:15:52 +0900

    feat: FCM 메시지 수신 로깅 구현

--- app/src/main/java/com/nexters/fooddiary/push/FoodDiaryFirebaseMessagingService.kt
+++ app/src/main/java/com/nexters/fooddiary/push/FoodDiaryFirebaseMessagingService.kt
@@ -2,8 +2,40 @@ package com.nexters.fooddiary.push
 
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class FoodDiaryFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+
+        val dataPayload = if (message.data.isEmpty()) "{}" else {
+            message.data.entries.joinToString(
+                prefix = "{",
+                postfix = "}",
+                separator = ", "
+            ) { (key, value) -> "\"$key\":\"$value\"" }
+        }
+
+        val notification = message.notification
+        Log.i(
+            TAG,
+            """
+            FCM message received
+            from=${message.from}
+            messageId=${message.messageId}
+            collapseKey=${message.collapseKey}
+            sentTime=${message.sentTime}
+            ttl=${message.ttl}
+            priority=${message.priority}
+            originalPriority=${message.originalPriority}
+            notification.title=${notification?.title}
+            notification.body=${notification?.body}
+            notification.channelId=${notification?.channelId}
+            data=$dataPayload
+            """.trimIndent()
+        )
+    }
+
     override fun onNewToken(token: String) {
         super.onNewToken(token)
         // TODO: 서버 토큰 동기화 API가 준비되면 여기서 업로드 처리
