60d6c49195ca41f4b357d3aa6018f738b522153f
Hoseong-Ryu <hoseong091234@gmail.com>
2026-02-18 10:50:09 +0900

    fix: develop merge하면서 진행안된 import 추가

--- presentation/home/src/main/java/com/nexters/fooddiary/presentation/home/HomeScreen.kt
+++ presentation/home/src/main/java/com/nexters/fooddiary/presentation/home/HomeScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonColors
