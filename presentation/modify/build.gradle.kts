39efc79ebd051090bb73c1863d96e0d354c8aff9
soyeonLee126 <soyeon23949@gmail.com>
2026-02-27 01:32:51 +0900

    - modify 모듈 debug implement 충돌 수정

--- presentation/modify/build.gradle.kts
+++ presentation/modify/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.ui.tooling.preview)
     debugImplementation(libs.androidx.compose.ui.tooling)
 
     implementation(libs.androidx.lifecycle.runtime.ktx)
